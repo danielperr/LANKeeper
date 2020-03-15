4ca746b39124525d06e7affe25697767dcf350cf
danielperr <daniel.perez079@gmail.com>
2020-03-15 15:15:27 +0200

    Add titles to panels and main window

--- lankeeper/gui.py
+++ lankeeper/gui.py
@@ -7,21 +7,22 @@ from PyQt5.QtGui import *
 
 import sys
 
-LAYOUT_PANEL_SPACING = 10
-LAYOUT_PANEL_MARGINS = 18, 10, 18, 10
-
 MIN_SIZE = 900, 720
 TITLE_FRAME_HEIGHT = 50
 DASHBOARD_PANEL_HEIGHT = 200
 
 COL_LANKEEPER_BLUE = '#1760B3'
 COL_PRIMARY_GRAY = '#EEEEEE'
+COL_PRIMATY_TEXT = '#333333'
+
+SRC_BANNER_WHITE = './resources/images/banner-white.png'
 
 
 class MainWindow (QMainWindow):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
         self.setWindowTitle('LANKeeper')
         self.setMinimumSize(*MIN_SIZE)
         centralWidget = QWidget()
@@ -33,16 +34,23 @@ class MainWindow (QMainWindow):
         # <titleFrame>
         self.titleFrame = QFrame()
         self.centralWidget().layout().addWidget(self.titleFrame)
-        self.titleFrame.setPalette(QPalette(QColor(COL_LANKEEPER_BLUE)))
-        self.titleFrame.setAutoFillBackground(True)
+        self.titleFrame.setLayout(QHBoxLayout())
+        self.titleFrame.layout().setContentsMargins(20, 0, 0, 0)
+        self.titleFrame.setStyleSheet('background-color: %s;' % COL_LANKEEPER_BLUE)
         self.titleFrame.setMinimumHeight(TITLE_FRAME_HEIGHT)
         self.titleFrame.setMaximumHeight(TITLE_FRAME_HEIGHT)
+        #   <titleLabel>
+        self.titleLabel = QLabel()
+        self.titleFrame.layout().addWidget(self.titleLabel)
+        titlePixmap = QPixmap(SRC_BANNER_WHITE)
+        titlePixmap = titlePixmap.scaled(128, 27, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        self.titleLabel.setPixmap(titlePixmap)
+        #   </titleLabel>
         # </titleFrame>
         # <mainFrame>
         self.mainFrame = QFrame()
         self.centralWidget().layout().addWidget(self.mainFrame)
-        self.mainFrame.setPalette(QPalette(QColor(COL_PRIMARY_GRAY)))
-        self.mainFrame.setAutoFillBackground(True)
+        self.mainFrame.setStyleSheet('background-color: %s;' % COL_PRIMARY_GRAY)
         self.mainFrame.setLayout(QVBoxLayout())
         self.mainFrame.layout().setContentsMargins(0, 10, 0, 0)
         self.mainFrame.layout().setSpacing(0)
@@ -57,6 +65,7 @@ class MainWindow (QMainWindow):
         #     <dashboardPanel>
         self.dashboardPanel = Panel()
         self.dashboardFrame.layout().addWidget(self.dashboardPanel)
+        self.dashboardPanel.panelTitle = 'Dashboard'
         self.dashboardPanel.setMinimumHeight(DASHBOARD_PANEL_HEIGHT)
         self.dashboardPanel.setMaximumHeight(DASHBOARD_PANEL_HEIGHT)
         #     </dashboardPanel>
@@ -65,16 +74,17 @@ class MainWindow (QMainWindow):
         self.devmonFrame = QFrame()
         self.mainFrame.layout().addWidget(self.devmonFrame)
         self.devmonFrame.setLayout(QHBoxLayout())
-        devmonFrameMargins = 0, LAYOUT_PANEL_MARGINS[1], 0, LAYOUT_PANEL_MARGINS[3]  # only left and right
         self.devmonFrame.layout().setContentsMargins(18, 0, 18, 10)
         self.devmonFrame.layout().setSpacing(10)
         #     <devicesPanel>
         self.devicesPanel = Panel()
         self.devmonFrame.layout().addWidget(self.devicesPanel, 12)
+        self.devicesPanel.panelTitle = 'Devices'
         #     </devicesPanel>
         #     <monitoringPanel>
         self.monitoringPanel = Panel()
         self.devmonFrame.layout().addWidget(self.monitoringPanel, 5)
+        self.monitoringPanel.panelTitle = 'Monitoring'
         #     </monitoringPanel>
         #   </devmonFrame>
         # </mainFrame>
@@ -85,11 +95,45 @@ class Panel (QFrame):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # rounded borders
+        self._panelTitle = 'Panel'
+
+        self.setLayout(QVBoxLayout())
+        self.layout().setContentsMargins(12, 0, 12, 0)
         self.setStyleSheet('background-color: white; border-radius: 2px;')
-        # shadow effect
         shadow = QGraphicsDropShadowEffect(blurRadius=10, xOffset=0, yOffset=3, color=QColor(0, 0, 0, 0.2*255))
         self.setGraphicsEffect(shadow)
+        # <titleFrame>
+        self.titleFrame = QFrame()
+        self.layout().addWidget(self.titleFrame)
+        self.titleFrame.setLayout(QHBoxLayout())
+        self.titleFrame.layout().setContentsMargins(0, 0, 0, 0)
+        self.titleFrame.setMinimumHeight(50)
+        self.titleFrame.setMaximumHeight(50)
+        self.titleFrame.setStyleSheet('border-bottom: 1px solid %s; border-radius: revert;' % COL_PRIMARY_GRAY)
+        #   <titleLabel>
+        self.titleLabel = QLabel()
+        self.titleFrame.layout().addWidget(self.titleLabel)
+        self.titleLabel.setText(self.panelTitle)
+        self.titleLabel.setAlignment(Qt.AlignLeft | Qt.AlignTop)
+        titleLabelFont = QFont('Segoe UI', 20, QFont.DemiBold)
+        titleLabelFont.setStyleStrategy(QFont.PreferAntialias)
+        self.titleLabel.setFont(titleLabelFont)
+        self.titleLabel.setStyleSheet('color: %s' % COL_PRIMATY_TEXT)
+        #   </titleLabel>
+        # </titleFrame>
+        # <mainFrame>
+        self.mainFrame = QFrame()
+        self.layout().addWidget(self.mainFrame)
+        # </mainFrame>
+
+    @property
+    def panelTitle(self):
+        return self._panelTitle
+
+    @panelTitle.setter
+    def panelTitle(self, value):
+        self.titleLabel.setText(value)
+        self._panelTitle = value
 
 
 if __name__ == '__main__':
